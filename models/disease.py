75a5a56cd3f918e81cd37ce47653e8a715894fd3
aditya-100 <52001350+aditya-100@users.noreply.github.com>
2020-05-09 21:17:28 +0530

    Update DB

--- models/disease.py
+++ models/disease.py
@@ -1,5 +1,6 @@
 from datastore import db
 
 class Disease(db.Model):
-	id = db.Column(db.Integer, primary_key= True)
-	name = db.Column(db.String)
+	id = db.Column(db.Integer, primary_key= True,autoincrement=True)
+	name = db.Column(db.String,nullable=False)
+    
