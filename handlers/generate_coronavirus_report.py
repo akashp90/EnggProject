2486f379e222947ef5389c4f3642a44f19ce5694
aditya-100 <52001350+aditya-100@users.noreply.github.com>
2020-05-15 11:36:49 +0530

    Update generate_coronavirus_report.py

--- handlers/generate_coronavirus_report.py
+++ handlers/generate_coronavirus_report.py
@@ -9,12 +9,15 @@ def gen_coronavirus_report(launch_method,al):
     get_table_in_html('covid_19_india.csv')
     get_moving_average_growth_rate_and_prediction('covid_19_india.csv')
     currtime=datetime.strptime(datetime.now().strftime("%Y %m %d 00 00 00"),"%Y %m %d %H %M %S")
-    filename="coronavirus_reports/"+currtime.strftime("%Y-%m-%d_00-00-00")+".html"
+    filename="/coronavirus_reports/"+currtime.strftime("%Y-%m-%d_00-00-00")+".html"
     filename1=os.getcwd()+filename
-    if(al==None):
-        rep=reports(ReportTime=currtime,ReportLoc=filename)
-    else:
-        rep=reports(ReportTime=currtime,ReportLoc=filename,Algorithm=al)
-    db.session.add(rep)
-    db.session.commit()
-    print("Reports generated")
+    try:
+        fd=open(filename1,'w')
+        fd.write(render_template('corona_virus_report_template',reportdate=currtime.strftime("%Y-%m-%d_00-00-00")))
+        fd.close()
+        rep=reports(ReportTime=currtime,ReportLoc=filename,Algorithm="Coronavirus")
+        db.session.add(rep)
+        db.session.commit()
+    except FileExistsError as e:
+        print("File Exists",e)
+    
