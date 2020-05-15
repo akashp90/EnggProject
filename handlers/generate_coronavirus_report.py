035e2c8115add90b291bc961923081b4650ebce8
aditya-100 <52001350+aditya-100@users.noreply.github.com>
2020-05-15 11:30:36 +0530

    Bug fixes

--- handlers/generate_coronavirus_report.py
+++ handlers/generate_coronavirus_report.py
@@ -9,10 +9,12 @@ def gen_coronavirus_report(launch_method,al):
     get_table_in_html('covid_19_india.csv')
     get_moving_average_growth_rate_and_prediction('covid_19_india.csv')
     currtime=datetime.strptime(datetime.now().strftime("%Y %m %d 00 00 00"),"%Y %m %d %H %M %S")
+    filename="coronavirus_reports/"+currtime.strftime("%Y-%m-%d_00-00-00")+".html"
+    filename1=os.getcwd()+filename
     if(al==None):
-        rep=reports(ReportTime=currtime,ReportLoc=filename1)
+        rep=reports(ReportTime=currtime,ReportLoc=filename)
     else:
-        rep=reports(ReportTime=currtime,ReportLoc=filename1,Algorithm=al)
+        rep=reports(ReportTime=currtime,ReportLoc=filename,Algorithm=al)
     db.session.add(rep)
     db.session.commit()
     print("Reports generated")
